4f150223685844a698762ed31f4d95508d6813da
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-18 21:23:54 +0800

    add UnlimitedStreamPromises

--- async/unlimited.go
+++ async/unlimited.go
@@ -2,7 +2,10 @@ package async
 
 import (
 	"context"
+	"errors"
 	"github.com/brickingsoft/rxp"
+	"github.com/brickingsoft/rxp/pkg/rate/spin"
+	"sync/atomic"
 )
 
 type unlimitedSubmitter struct {
@@ -30,5 +33,31 @@ func UnlimitedStreamPromise[R any](ctx context.Context, buf int) (promise Promis
 	exec := rxp.From(ctx)
 	submitter := &unlimitedSubmitter{exec: exec}
 	promise = newStreamPromise[R](ctx, submitter, buf)
+	return
+}
+
+// UnlimitedStreamPromises
+// 不受 rxp.Executors 最大协程限制的并行流。
+//
+// 当所有未来都结束了才会通知一个 context.Canceled 作为整体结束。
+func UnlimitedStreamPromises[R any](ctx context.Context, size int, buf int) (v Promise[R], err error) {
+	if size < 1 {
+		err = errors.New("async: stream promises size < 1")
+		return
+	}
+	ss := &streamPromises[R]{
+		members:  make([]Promise[R], size),
+		index:    0,
+		size:     size,
+		alive:    new(atomic.Int64),
+		canceled: false,
+		locker:   spin.New(),
+	}
+	for i := 0; i < size; i++ {
+		s := UnlimitedStreamPromise[R](ctx, buf)
+		ss.members[i] = s
+	}
+	ss.alive.Store(int64(size))
+	v = ss
 	return
 }
