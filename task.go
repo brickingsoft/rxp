853796fb124457db38aa36c5251f9513a21f5bc7
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-18 18:42:03 +0800

    mod submitter

--- task.go
+++ task.go
@@ -14,18 +14,26 @@ type Task func()
 type TaskSubmitter interface {
 	// Submit
 	// 提交一个任务
-	Submit(task Task)
+	Submit(task Task) (ok bool)
 }
 
 type submitterImpl struct {
 	lastUseTime time.Time
 	ch          chan Task
+	running     *atomic.Bool
 	tasks       *atomic.Int64
 }
 
-func (submitter *submitterImpl) Submit(task Task) {
-	submitter.ch <- task
-	if task != nil {
-		submitter.tasks.Add(1)
+func (submitter *submitterImpl) Submit(task Task) (ok bool) {
+	if task == nil {
+		submitter.ch <- task
+		ok = true
+		return
 	}
+	if submitter.running.Load() {
+		submitter.ch <- task
+		submitter.tasks.Add(1)
+		ok = true
+	}
+	return
 }
