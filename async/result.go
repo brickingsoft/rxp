9addd1d76c5a16e4c783db2c41b73f908d37cc31
wangminxiang <ryougi.nevermore@hotmail.com>
2024-11-19 19:16:37 +0800

    use pool manged

--- async/result.go
+++ async/result.go
@@ -2,8 +2,12 @@ package async
 
 import (
 	"context"
+	"github.com/brickingsoft/rxp/pkg/rate/spin"
 	"io"
 	"reflect"
+	"runtime"
+	"sync"
+	"time"
 )
 
 // Void
@@ -54,6 +58,8 @@ func (r result[E]) Cause() error {
 // err: 来自 Promise.Failed() 的错误，或者 Promise.Cancel() 的结果，也可能是 Context.Err() 。
 type ResultHandler[E any] func(ctx context.Context, entry E, cause error)
 
+var DiscardVoidHandler ResultHandler[Void] = func(_ context.Context, _ Void, _ error) {}
+
 func tryCloseResultWhenUnexpectedlyErrorOccur(v any) {
 	if v == nil {
 		return
@@ -63,4 +69,104 @@ func tryCloseResultWhenUnexpectedlyErrorOccur(v any) {
 	if isCloser {
 		_ = closer.Close()
 	}
+}
+
+type genericResultChanCancel struct{}
+
+type genericResultChanEntry struct {
+	value any
+	cause error
+}
+
+type genericResultChan struct {
+	entries chan any
+	timer   *time.Timer
+	stream  bool
+	closed  bool
+	lock    sync.Locker
+}
+
+func (grc *genericResultChan) IsClosed() bool {
+	grc.lock.Lock()
+	defer grc.lock.Unlock()
+	return grc.closed
+}
+
+func (grc *genericResultChan) Close() {
+	grc.lock.Lock()
+	if grc.closed {
+		grc.lock.Unlock()
+		return
+	}
+	grc.entries <- genericResultChanCancel{}
+	grc.closed = true
+	grc.lock.Unlock()
+}
+
+func (grc *genericResultChan) CloseByUnexpectedError() {
+	grc.lock.Lock()
+	if grc.closed {
+		grc.lock.Unlock()
+		return
+	}
+	grc.closed = true
+	grc.lock.Unlock()
+}
+
+func (grc *genericResultChan) Send(entry genericResultChanEntry) {
+	grc.lock.Lock()
+	if grc.closed {
+		tryCloseResultWhenUnexpectedlyErrorOccur(entry)
+		grc.lock.Unlock()
+		return
+	}
+	grc.entries <- entry
+	if !grc.stream {
+		grc.closed = true
+	}
+	grc.lock.Unlock()
+}
+
+func (grc *genericResultChan) SetTimeout(timeout time.Duration) {
+	grc.lock.Lock()
+	if grc.closed {
+		grc.lock.Unlock()
+		return
+	}
+	grc.timer.Reset(timeout)
+	grc.lock.Unlock()
+	return
+}
+
+var (
+	genericResultChanEntryBuffer  = runtime.NumCPU() * 2
+	genericResultChanTimerTimeout = 500 * time.Millisecond
+	genericResultChanPool         = sync.Pool{
+		New: func() interface{} {
+			grc := &genericResultChan{
+				entries: make(chan any, genericResultChanEntryBuffer),
+				timer:   time.NewTimer(genericResultChanTimerTimeout),
+				stream:  false,
+				closed:  false,
+				lock:    spin.New(),
+			}
+			grc.timer.Stop()
+			return grc
+		},
+	}
+)
+
+func getGenericResultChan() *genericResultChan {
+	grc := genericResultChanPool.Get().(*genericResultChan)
+	return grc
+}
+
+func putGenericResultChan(ch *genericResultChan) {
+	if ch == nil {
+		return
+	}
+	ch.stream = false
+	ch.closed = false
+	ch.timer.Stop()
+	genericResultChanPool.Put(ch)
 }
