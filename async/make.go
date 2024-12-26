e399e3d6269235f3bed54f78c777bf7d39883cf0
wangminxiang <ryougi.nevermore@hotmail.com>
2024-12-26 16:34:50 +0800

    mod

--- async/make.go
+++ async/make.go
@@ -177,6 +177,7 @@ func Make[R any](ctx context.Context, options ...Option) (p Promise[R], err erro
 			times--
 			if times < 0 {
 				times = 10
+				time.Sleep(time.Millisecond)
 				runtime.Gosched()
 			}
 		}
