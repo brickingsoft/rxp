57a25a63748713c80987413a23f8ef117f7d881c
wangminxiang <ryougi.nevermore@hotmail.com>
2025-02-08 21:29:40 +0800

    mod

--- async/await.go
+++ async/await.go
@@ -2,6 +2,7 @@ package async
 
 import (
 	"context"
+	"github.com/brickingsoft/errors"
 )
 
 // Awaitable
@@ -12,37 +13,40 @@ type Awaitable[R any] interface {
 	Await() (r R, err error)
 }
 
-func AwaitableFuture[R any](future Future[R]) (af Awaitable[R]) {
-	ch := make(chan Result[R], 1)
-	var handler ResultHandler[R] = func(ctx context.Context, r R, err error) {
-		if IsCanceled(err) {
-			close(ch)
-			return
-		}
-		ch <- result[R]{
-			value: r,
-			err:   err,
-		}
-	}
-	future.OnComplete(handler)
-	af = &awaitableFuture[R]{
+// AwaitableFuture
+// 转换为同步等待
+func AwaitableFuture[R any](future Future[R]) Awaitable[R] {
+	af := &awaitableFuture[R]{
 		future: future,
-		ch:     ch,
+		ch:     make(chan result[R], 1),
 	}
-	return
+	future.OnComplete(af.handle)
+	return af
 }
 
 type awaitableFuture[R any] struct {
 	future Future[R]
-	ch     chan Result[R]
+	ch     chan result[R]
 }
 
 func (af *awaitableFuture[R]) Await() (r R, err error) {
 	ar, ok := <-af.ch
 	if !ok {
-		err = Canceled
+		err = errors.From(Canceled, errors.WithMeta(errMetaPkgKey, errMetaPkgVal))
 		return
 	}
 	r, err = ar.Value(), ar.Error()
 	return
+}
+
+func (af *awaitableFuture[R]) handle(_ context.Context, r R, err error) {
+	ch := af.ch
+	if IsCanceled(err) {
+		close(ch)
+		return
+	}
+	ch <- result[R]{
+		value: r,
+		err:   err,
+	}
 }
--- async/await_test.go
+++ async/await_test.go
@@ -4,8 +4,37 @@ import (
 	"github.com/brickingsoft/rxp"
 	"github.com/brickingsoft/rxp/async"
 	"sync"
+	"sync/atomic"
 	"testing"
 )
+
+func TestAwait(t *testing.T) {
+	exec, execErr := rxp.New()
+	if execErr != nil {
+		t.Fatal(execErr)
+		return
+	}
+	ctx := exec.Context()
+	defer func() {
+		err := exec.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	promise, ok := async.Make[int](ctx)
+	if ok != nil {
+		t.Errorf("try promise failed")
+		return
+	}
+	promise.Succeed(1)
+	aw := async.AwaitableFuture[int](promise.Future())
+	v, err := aw.Await()
+	if err != nil {
+		t.Errorf("await failed: %v", err)
+	}
+	t.Log(v)
+}
 
 func TestPromise_Await(t *testing.T) {
 	exec, execErr := rxp.New()
@@ -54,13 +83,13 @@ func TestStreamPromise_Await(t *testing.T) {
 		t.Errorf("try promise failed")
 		return
 	}
-	af := async.AwaitableFuture[int](promise.Future())
 	wg := new(sync.WaitGroup)
 	wg.Add(1)
-	go func(af async.Awaitable[int], wg *sync.WaitGroup) {
+	aw := async.AwaitableFuture[int](promise.Future())
+	go func(aw async.Awaitable[int], wg *sync.WaitGroup) {
 		defer wg.Done()
 		for {
-			v, err := af.Await()
+			v, err := aw.Await()
 			if err != nil {
 				if async.IsCanceled(err) {
 					return
@@ -71,10 +100,44 @@ func TestStreamPromise_Await(t *testing.T) {
 			t.Log("await:", v)
 		}
 
-	}(af, wg)
+	}(aw, wg)
 	for i := 0; i < 10; i++ {
 		promise.Succeed(i)
 	}
 	promise.Cancel()
 	wg.Wait()
+}
+
+func BenchmarkAwaitableFuture(b *testing.B) {
+	b.ReportAllocs()
+
+	exec, execErr := rxp.New()
+	if execErr != nil {
+		b.Fatal(execErr)
+		return
+	}
+	ctx := exec.Context()
+
+	failed := new(atomic.Int64)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			promise, err := async.Make[int](ctx)
+			if err != nil {
+				failed.Add(1)
+				return
+			}
+			aw := async.AwaitableFuture[int](promise.Future())
+			promise.Succeed(1)
+			if _, err = aw.Await(); err != nil {
+				failed.Add(1)
+				return
+			}
+		}
+	})
+	err := exec.Close()
+	if err != nil {
+		b.Error(err)
+	}
+	b.ReportMetric(float64(failed.Load()), "failed")
 }
